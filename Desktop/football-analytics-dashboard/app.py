1b3dfcdb549c2929798ad9e68242f676ea589210
Manbyen Binkat <loisnansel@gmail.com>
2026-06-18 11:26:57 +0100

    Master Input Business Logic Gateway Controller

--- Desktop/football-analytics-dashboard/app.py
+++ Desktop/football-analytics-dashboard/app.py
@@ -11,17 +11,27 @@ class InputGatewayController:
 
         if not clean_name: 
             return 'Error: Name field blank.'
+        
         is_new = self.storage.commit_bookmark(clean_name)
+
         if is_new:
             return 'Success: Team bookmarked!'
         else:
             return 'Already favorited.'
     
-    def process_diary_note_request(self,home: str, away: str, date: str, raw_note: str) -> str:
+    def process_diary_note_request(
+        self,
+        home: str, 
+        away: str, 
+        date: str, 
+        raw_note: str
+    ) -> str:
         
         if not raw_note or not raw_note.strip(): 
             return 'Rejected: Notes field empty'
+        
         key = f'{home.strip().lower()}_{away.stri().lower()}_{date}'.replace(' ','_')
+
         self.storage.commit_match_note(key, raw_note)
 
         return 'Success: Commentary committed to system disk storage files!'
