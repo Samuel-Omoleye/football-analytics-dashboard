e69d8e3cccf6e079b60a811edc9bd07983f707eb
Samuel Omoleye <omoleyesamuel11@gmail.com>
2026-06-18 23:38:52 +0100

    added classes

--- Desktop/football-analytics-dashboard/app_core.py
+++ Desktop/football-analytics-dashboard/app_core.py
@@ -23,4 +23,17 @@ def initialize_project_environment():
         else:
             print(f'[OK] Validated tracking file asset signature: {asset}')
 
+##OOP: Using classes to hold related info together to avoid parsing random variables
 
+class Team:
+    def __init__(self, name: str, trivia: list, recent_form: str):
+        self.name = name                 
+        self.trivia = trivia             
+        self.recent_form = recent_form   
+
+class Match:
+    def __init__(self, home_team: str, away_team: str, date: str, score: str = None):
+        self.home_team = home_team       
+        self.away_team = away_team       
+        self.date = date                 
+        self.score = score               
