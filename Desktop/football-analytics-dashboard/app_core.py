731ef2d9963301e37a8cbf931fa6d06868f2dfb8
David Umoh <davtido@gmail.com>
2026-06-18 18:24:54 +0100

    Completed my assigned module

--- Desktop/football-analytics-dashboard/app_core.py
+++ Desktop/football-analytics-dashboard/app_core.py
@@ -21,3 +21,14 @@ def initialize_project_environment():
             print(f'[OK] Validated tracking file asset signature: {asset}')
 
 
+class MatchAnalyzer:
+    def calculate_fun_forecast(self, home_team: Team, away_team: Team) -> str:
+        form_weights = {'W': 3, 'D': 1, 'L': 0}
+
+        home_points = sum(form_weights.get(res, 0) for res in home_team.recent_form.split(','))
+        away_points = sum(form_weights.get(res, 0) for res in away_team.recent_form.split(','))
+        disclaimer = '(Playful estimate only o!)'
+
+        if home_points > away_points:
+            return f'Forecast: Advantage {home_team.name}! {disclaimer}'
+        return f'Forecast: Close match bound for draw! {disclaimer}'
